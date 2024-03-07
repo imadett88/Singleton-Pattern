1e8930dc9595c015e7ceb625c2856b8f4d52d53e
imadett88 <imadettamen18@.com>
2024-03-07 09:35:28 +0100

    FC

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SingletonPattern;
+
+
+Thread tr = new Thread(() => {
+    var instance = UpService.GetUpService(1);
+ });
+
+Thread te = new Thread(() => {
+    var instance = UpService.GetUpService(2);
+});
+
+tr.Start();
+te.Start();
+
+tr.Join();
+te.Join();
---
+++ SingletonPattern/UpService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+   class UpService
+    {
+        private UpService(){}
+
+        public int Id { get; private set; }
+
+        private static UpService _instance;
+        private static readonly object _lock = new object();
+
+         public static UpService GetUpService(int id)
+        {
+            if(_instance == null)
+            {
+                lock (_lock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new UpService();
+                        _instance.Id = id;
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
